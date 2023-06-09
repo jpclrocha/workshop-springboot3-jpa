368714c9fa759998fc7bdd02dc306d7719fdef59
jpclrocha <jpclrocha@gmail.com>
2023-06-09 18:09:45 -0300

    Payment, one-to-one association

--- src/main/java/com/jopezin/course/entities/Order.java
+++ src/main/java/com/jopezin/course/entities/Order.java
@@ -28,6 +28,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -53,23 +56,31 @@ public class Order implements Serializable {
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
-    public OrderStatus getOrderStatus(){return OrderStatus.valueOf(orderStatus);}
 
+    public OrderStatus getOrderStatus(){return OrderStatus.valueOf(orderStatus);}
     public void setOrderStatus(OrderStatus orderStatus){
         if(orderStatus != null){
             this.orderStatus = orderStatus.getCode();
         }
     }
+
     public User getClient() {
         return client;
     }
-
     public void setClient(User client) {
         this.client = client;
     }
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Double total(){
