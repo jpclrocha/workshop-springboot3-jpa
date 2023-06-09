e8bcf0495097d88fad816d0ca271f3f8e85243cb
jpclrocha <jpclrocha@gmail.com>
2023-06-09 19:21:34 -0300

    Exception handling - update method

--- src/main/java/com/jopezin/course/services/UserService.java
+++ src/main/java/com/jopezin/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.jopezin.course.entities.User;
 import com.jopezin.course.repositories.UserRepository;
 import com.jopezin.course.services.exceptions.DatabaseException;
 import com.jopezin.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj){
