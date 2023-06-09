dd044270eb26b8cdfa9d566ffff503635975ddbe
jpclrocha <jpclrocha@gmail.com>
2023-06-09 19:07:45 -0300

    Exception handling - findById

---
+++ src/main/java/com/jopezin/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jopezin.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
