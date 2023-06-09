61da4b4a63c31b4d67a615eaa6ea936dbc746443
jpclrocha <jpclrocha@gmail.com>
2023-06-09 19:58:13 -0300

    Dev profile config using postgres + docker

--- src/main/java/com/jopezin/course/services/UserService.java
+++ src/main/java/com/jopezin/course/services/UserService.java
@@ -49,7 +49,6 @@ public class UserService {
         }catch (EntityNotFoundException e){
             throw new ResourceNotFoundException(id);
         }
-
     }
 
     private void updateData(User entity, User obj){
